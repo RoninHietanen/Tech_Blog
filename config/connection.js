7889f287b9e91b23410c91f7d91576c82c7f1f71
RoninHietanen <84194778+RoninHietanen@users.noreply.github.com>
2021-09-04 03:30:44 +1000

    trying to fix connection

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,8 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: 3306
   });
 }
 
