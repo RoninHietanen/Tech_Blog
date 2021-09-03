835fd0d678888d0ee0a16e21fbc123aa4f831805
RoninHietanen <84194778+RoninHietanen@users.noreply.github.com>
2021-09-04 01:55:36 +1000

    fixing connection

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({helpers});
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
